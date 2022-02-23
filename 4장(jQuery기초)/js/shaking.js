367e5a3dd8e061ae4ef675101f77f06a9fc47273
dlckdgks99 <89891838+dlckdgks99@users.noreply.github.com>
2022-02-23 23:41:06 +0900

    Add files via upload

---
+++ 4장(jQuery기초)/js/shaking.js
@@ -0,0 +1,8 @@
+<script>
+    function hello() {
+        var rnd1 = Math.floor(Math.random()*20);
+        var rnd2=Math.floor(Math.random()*40);
+        var rnd3=Math.floor(Math.random()*3)+100;
+        }
+
+</script>
